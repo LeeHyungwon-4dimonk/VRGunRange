08c1dd880dcc9583fa93a964da73197c23c479b2
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-06-15 15:27:19 +0900

    [Feat] Game End(Quit)
    게임을 종료할 수 있는 UI와 버튼을 만듬

---
+++ Assets/Scripts/GameSettings/GameEndComponent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndComponent : MonoBehaviour
+{
+    public void GameEnd()
+    {
+        Application.Quit();
+    }
+}
